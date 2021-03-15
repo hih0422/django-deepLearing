967dbef75e88cc5f2dc598c8800a3bb0492c24cc
seongmin kim <hih1216@hmcorp.co.kr>
2021-03-15 14:29:39 +0900

    bootstrap dropdown nav header

--- conf/settings.py
+++ conf/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'conf.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'postgres',
+        'NAME': 'test_db',
         'USER': 'postgres',
         'PASSWORD': 'qwe123',
         'HOST': 'localhost',
@@ -123,11 +123,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'deep_learning','static'),
+]
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+)
+
+STATIC_ROOT = os.path.join(BASE_DIR, '.static_root')
 
 
 # Media files for file upload
--- deep_learning/models.py
+++ deep_learning/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class FirstTest(models.Model):
+    test_1 = models.CharField(max_length=70)
+    test_2 = models.IntegerField
+    test_3 = models.FloatField
+    test_4 = models.DateTimeField
+
+    def __str__(self):
+        return self.test_1
